358503a7f060dd99f6dfed77f64455f6c9009a04
Rae Zhang <junweizhang130@Gmail.com>
2023-02-16 15:58:44 -0500

    the simulation of data and graph in Figure 4

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -7,13 +7,53 @@
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
 
-
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(haven)
 
-#### Simulate data ####
-# [...ADD CODE HERE...]
+datadescriptive <- read_dta("inputs/data/datadescriptive.dta")
+head(datadescriptive)
 
+liquidity <- read_dta("inputs/data/liquidity.dta")
+head(liquidity)
 
+#### Simulate data of Figure 4 ####
+set.seed(853)
 
+# create a data frame with all combinations of years and countries
+years <- 1531:1790
+countries <- c("England/GB", 
+               "Germany", 
+               "Holland", 
+               "Italy", 
+               "Portugal", 
+               "Spain"
+               )
+
+simulated_data <- 
+  expand_grid(Years = years, Countries = countries)
+
+# group by year and sample one country per group
+simulated_data <- 
+  simulated_data |>
+  group_by(Years) |>
+  sample_n(size = 6) |>
+  ungroup() |>
+  mutate(nominal_gdp = round(runif(n(), 
+                                      min = 1.662095, 
+                                      max = 5.343219), 
+                                digits = 6))
+
+head(simulated_data)
+
+# simulate the graphs
+library(ggplot2)
+
+# plot
+ggplot(simulated_data, aes(x = Years, y = nominal_gdp, color = Countries)) +
+  geom_line() +
+  labs(x = "Year", 
+       y = "Nominal GDP", 
+       title = "Nominal GDP for 6 Countries") +
+  theme_minimal() +
+  facet_wrap(~ Countries, ncol = 2)
