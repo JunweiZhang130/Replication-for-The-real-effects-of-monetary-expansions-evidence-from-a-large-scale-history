f9b58927d6fff2b0be24b5f7665a1f564df3a0fc
Rae Zhang <junweizhang130@Gmail.com>
2023-02-22 15:22:58 -0500

    Final Revision

---
+++ scripts/02-test_data.R
@@ -0,0 +1,15 @@
+#### Preamble ####
+# Purpose: Tests Data from Replication-for-The-real-effects-of-monetary-expansions-evidence-from-a-large-scale-history
+# Author: Rae Zhang / Jenny Shen / Sarah Zhang
+# Data: 22 February 2023 [...UPDATE THIS...]
+# Contact: junwei.zhang@mail.utoronto.ca 
+# License: MIT
+# Pre-requisites: Have access to the paper and reproduction package
+# Any other information needed? N/A
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Test data ####
